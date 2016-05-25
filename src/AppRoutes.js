90042dc5bfd2295f7375642e6e1a7681ce584515
Jorge <me.mark.two@gmail.com>
2016-05-25 16:17:15 -0400

    feat: use browserHistory and add index route

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Router, Route, hashHistory } from 'react-router'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from "./layouts/App"
 import About from "./layouts/About"
+import Login from "./layouts/Login"
 
 export default React.createClass({
   render() {
     return (
-      <Router history={hashHistory}>
-		    <Route path="/" component={App}>
-		      <Route path="/about" component={About}/>
+      <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Login}/>
+          <Route path="/about" component={About}/>
         </Route>
-		  </Router>
+      </Router>
       )
   }
 })
